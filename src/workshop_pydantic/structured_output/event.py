c911d8d5efb67b29c46a6066411124621f70f945
Paul <paul.lachaise19@gmail.com>
2025-07-09 11:01:49 +0200

    feat/ add structured_output section

---
+++ src/workshop_pydantic/structured_output/event.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, computed_field
+from datetime import datetime
+
+from .people import Member, Freelancer, Researcher
+from .enums import EventType
+
+
+class Event(BaseModel):
+    name: str
+    event_type: EventType
+    registrants: list[Member | Freelancer | Researcher]
+    location: str
+    start_time: datetime
+    end_time: datetime
+
+    @computed_field
+    @property
+    def register_count(self) -> int:
+        return len(self.registrants)
---
+++ src/workshop_pydantic/structured_output/generate_club.py
@@ -0,0 +1,17 @@
+from workshop_pydantic.structured_output.club import Club
+import openai
+import json
+from dotenv import load_dotenv
+
+
+def main():
+    load_dotenv()
+    client = openai.OpenAI()
+    ...
+    ...
+    ...
+
+
+
+if __name__ == "__main__":
+    main()
